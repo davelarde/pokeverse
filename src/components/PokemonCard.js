75d88c61dcfee207ff2fcf0217e44c9acd33fc78
davelarde <danielavelarde4@gmail.com>
2023-01-06 17:08:47 -0500

    adding pokemon card fetching request with useEffect

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function PokemonCard({ url, name }) {
+function PokemonCard({ url, name, setPokemons, filteredPokemons }) {
+  const [urlData, setUrlData] = useState([])
+  useEffect(()=>{
+    fetch(url)
+    .then((res)=> res.json())
+    .then((data)=>{
+      setUrlData(data);
+    });
+  },[filteredPokemons]);
+  
 
   return (
     <div>
